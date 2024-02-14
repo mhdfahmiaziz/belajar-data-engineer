d5d3f8b3f9e4438dad56d51292e7a9ff964dd0fe
Mhd Fahmi Aziz <99966764+mhdfahmiaziz@users.noreply.github.com>
2024-02-14 16:45:19 +0700

    Create to_gcs.py

---
+++ prefect/prefect-demo/to_gcs.py
@@ -0,0 +1,68 @@
+from pathlib import Path
+import pandas as pd
+from prefect import flow, task
+from prefect_gcp.cloud_storage import GcsBucket
+from random import randint
+
+
+@task(retries=3, log_prints=True)
+def fetch(dataset_url: str) -> pd.DataFrame:
+    """Read taxi data from web into pandas DataFrame"""
+
+    df = pd.read_csv(dataset_url)
+    return df
+
+
+@task(log_prints=True)
+def clean(df: pd.DataFrame) -> pd.DataFrame:
+    """Fix dtype issues"""
+    df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"])
+    df["tpep_dropoff_datetime"] = pd.to_datetime(df["tpep_dropoff_datetime"])
+    print(df.head(2))
+    print(f"columns: {df.dtypes}")
+    print(f"rows: {len(df)}")
+    return df
+
+
+@task(log_prints=True)
+def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
+    """Write DataFrame out locally as parquet file"""
+    directory_path = Path(f"data/{color}/")
+
+    # Create the directory if it doesn't exist
+    directory_path.mkdir(parents=True, exist_ok=True)
+
+    # Specify the file path
+    file_path = directory_path / f"{dataset_file}.parquet"
+
+    # Write the DataFrame to the parquet file
+    df.to_parquet(file_path, compression="gzip")
+
+    return file_path
+
+
+@task(log_prints=True)
+def write_gcs(path: Path) -> None:
+    """Upload local parquet file to GCS"""
+    gcs_block = GcsBucket.load("gcs-prefect")
+    gcs_block.upload_from_path(from_path=path, to_path=path)
+    return
+
+
+@flow(log_prints=True)
+def etl_web_to_gcs() -> None:
+    """The main ETL function"""
+    color = "yellow"
+    year = 2021
+    month = 1
+    dataset_file = f"{color}_tripdata_{year}-{month:02}"
+    dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
+
+    df = fetch(dataset_url)
+    df_clean = clean(df)
+    path = write_local(df_clean, color, dataset_file)
+    write_gcs(path)
+
+
+if __name__ == "__main__":
+    etl_web_to_gcs()
